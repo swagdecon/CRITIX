578a5022a2d7ca5c659ea5120b8eb9eddfd5909f
Connor.Pant <Connor.Pant@openbet.com>
2023-01-27 16:49:33 +0000

    adding other files

---
+++ backend/src/main/java/com/popflix/PopflixApplication.java
@@ -0,0 +1,13 @@
+package com.popflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopflixApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/popflix/PopflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
