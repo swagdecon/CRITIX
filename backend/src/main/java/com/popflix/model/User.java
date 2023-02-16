0a7183a0f00d36dc4f0d1fe80d7c5f968e603917
Connor Pant <56475499+swagdecon@users.noreply.github.com>
2023-02-16 21:53:14 +0000

    JWT WORKING

--- backend/src/main/java/com/popflix/model/User.java
+++ backend/src/main/java/com/popflix/model/User.java
@@ -1,90 +1,72 @@
 package com.popflix.model;
-import static java.lang.Boolean.TRUE;
-import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
 
-public class User {
- @Id
- private Long id;
- private String username;
- private String password;
- private boolean enabled;
- private String email;
- private String image;
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
 
- // Constructors:
- public User() {
-  this.enabled = TRUE;
- }
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
- public User(String username, String password, String email) {
-  this.username = username;
-  this.email = email;
-  this.password = password;
-  this.enabled = TRUE;
- }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
 
- public User(String username, String email, String password, boolean enabled) {
-  this.username = username;
-  this.email = email;
-  this.password = password;
-  this.enabled = enabled;
- }
+    @Override
+    public String getPassword() {
+        return password;
+    }
 
- public User(Long id, String username, String email) {
-  this.id = id;
-  this.username = username;
-  this.email = email;
- }
+    @Override
+    public String getUsername() {
+        return email;
+    }
 
- public User(String username, String email, String password, boolean enabled, String image) {
-  this.username = username;
-  this.email = email;
-  this.password = password;
-  this.enabled = enabled;
-  this.image = image;
- }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
- // Getters and Setters:
- public Long getId() {
-  return this.id;
- }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
- public void setId(Long id) {
-  this.id = id;
- }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
- public String getUsername() {
-  return this.username;
- }
-
- public String getPassword() {
-  return this.password;
- }
-
- public void setUsername(String username) {
-  this.username = username;
- }
-
- public void setPassword(String password) {
-  this.password = password;
- }
-
- public String getImage() {
-  return this.image;
- }
-
- public void setImage(String image) {
-  this.image = image;
- }
-
- public String getEmail() {
-  return this.email;
- }
-
- public void setEmail(String email) {
-  this.email = email;
- }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- backend/src/main/java/com/popflix/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.popflix.repository;
-import org.springframework.data.repository.CrudRepository;
-import com.popflix.model.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
- boolean existsByUsername(String username);
- User findByUsername(String username);
-}
