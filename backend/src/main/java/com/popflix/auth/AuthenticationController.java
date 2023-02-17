e5eb1b82102a8d58bc5cbda62312250976eb537d
Connor Pant <56475499+swagdecon@users.noreply.github.com>
2023-02-17 15:35:32 +0000

    small changes

--- backend/src/main/java/com/popflix/auth/AuthenticationController.java
+++ backend/src/main/java/com/popflix/auth/AuthenticationController.java
@@ -1,7 +1,6 @@
 package com.popflix.auth;
 
 import lombok.RequiredArgsConstructor;
-
 import org.apache.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
