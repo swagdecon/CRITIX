54ac6ed7792eabba762c22ec3026b1de104fdb65
Connor Pant <56475499+swagdecon@users.noreply.github.com>
2023-03-02 14:36:23 +0000

    404 functionality working

--- frontend/src/views/error.js
+++ frontend/src/views/error.js
@@ -3,50 +3,86 @@ import "../misc/error.css";
 
 function Error404() {
   return (
-    <div className="wrapper">
-      <div id="container">
-        <div className="error">
-          <div className="wrap">
-            <div className="404" style={{ fontSize: "30px" }}>
-              <pre>
-                <code>
-                  &lt;!
-                  <span className="red">DOCTYPE html</span>
-                  <span className="red">&gt;</span>
-                  <span className="blue">&lt;html</span>{" "}
-                  <span className="yellow">lang=</span>
-                  <span className="green">&quot;en&quot;</span>
-                  <span className="blue">&gt;</span>
-                  <span className="blue">&lt;body</span>{" "}
-                  <span className="yellow">class=</span>
-                  <span className="green">&quot;broken&quot;</span>
-                  <span className="blue">&gt;</span>
-                  <br />
-                  <br />
-                  ERROR 404! FILE NOT FOUND!
-                  <br />
-                  <br />
-                  <span className="comment">
-                    &lt;!-- page not found, time to go. --&gt;
-                  </span>
-                  <br />
-                  <br />
-                  <a href="/" style={{ textDecoration: "none" }}>
+    <div>
+      <link rel="stylesheet" href="/error.css" />
+      <div className="wrapper">
+        <div id="container">
+          <div className="error">
+            <div className="wrap">
+              <div className={404} style={{ fontSize: "30px" }}>
+                <pre>
+                  <code>
+                    {"\n"}
+                    {"  "}
+                    <span className="red">&lt;!</span>
+                    <span>DOCTYPE html</span>
                     <span className="red">&gt;</span>
-                    <input id="the_button" type="button" value="back home." />
-                  </a>
-                  <br />
-                  <br />
-                  <span className="blue">&nbsp;&lt;/body&gt;</span>
-                  <span className="blue">&lt;/html&gt;</span>
-                </code>
-              </pre>
+                    {"\n"}
+                    {"    "}
+                    <span className="blue">
+                      &lt;html <span className="yellow">lang=</span>
+                      <span className="green">&#34;en&#34;</span>&gt;
+                    </span>
+                    {"\n"}
+                    {"      "}
+                    <span className="blue">
+                      &lt;body <span className="yellow">class=</span>
+                      <span className="green">&#34;broken&#34;</span>&gt;
+                    </span>
+                    {"\n"}
+                    {"  "}
+                    {"\n"}
+                    {"      "}
+                    <span>
+                      ERROR 404! {"\n"}
+                      {"        "}FILE NOT FOUND!
+                    </span>
+                    {"\n"}
+                    {"        "}
+                    {"\n"}
+                    {"        "}
+                    <span className="comment">
+                      &lt;!-- page not found,{"\n"}
+                      {"        "}time to go.--&gt;
+                    </span>
+                    {"\n"}
+                    {"          "}
+                    {"\n"}
+                    {"        "}
+                    <a href="/" style={{ textDecoration: "none" }}>
+                      {"\n"}
+                      {"        "}
+                      <span className="red" style={{ textDecoration: "none" }}>
+                        &gt;
+                      </span>{" "}
+                      {"\n"}
+                      {"        "}
+                      <input
+                        id="the_button"
+                        type="button"
+                        defaultValue="back home."
+                      />
+                      {"\n"}
+                      {"        "}
+                    </a>
+                    {"\n"}
+                    {"      "}
+                    <span className="blue">&nbsp;&lt;/body&gt;</span>
+                    {"\n"}
+                    {"    "}
+                    <span className="blue">&lt;/html&gt;</span>
+                    {"\n"}
+                    {"    "}
+                  </code>
+                </pre>
+              </div>
             </div>
+            <span className="fnf" />
           </div>
-          <span className="fnf"></span>
         </div>
       </div>
     </div>
   );
 }
+
 export default Error404;
