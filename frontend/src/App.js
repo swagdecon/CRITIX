87afcf1213e6f06b75c4b6b96215da224fe6ae9c
Connor Pant <56475499+swagdecon@users.noreply.github.com>
2023-04-11 21:16:08 +0100

    fixed some dependency issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./index.js";
 import Login from "./views/login.js";
 import SignUp from "./views/signup.js";
@@ -16,24 +11,23 @@ import IndMovie from "./views/indMoviePage.js";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/home"
-          element={
-            <PrivateRoute>
-              <Homepage />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/api/movies/:endpoint/:id" element={<IndMovie />} />
-        <Route path="/api/movies/movie/:id" element={<IndMovie />} />
-        <Route path="/403" element={<Error403 />} />
-        <Route path="*" element={<Error404 />} />
-        <Route component={<Navigate replace to="/404" />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/home"
+        element={
+          <PrivateRoute>
+            <Homepage />
+          </PrivateRoute>
+        }
+      />
+      <Route path="/api/movies/:endpoint/:id" element={<IndMovie />} />
+      <Route path="/api/movies/movie/:id" element={<IndMovie />} />
+
+      <Route path="/403" element={<Error403 />} />
+      <Route path="*" element={<Error404 />} />
+      <Route component={<Navigate replace to="/404" />} />
+    </Routes>
   );
 }
--- frontend/src/components/Carousel/MovieCarousel/MovieCarousel.js
+++ frontend/src/components/Carousel/MovieCarousel/MovieCarousel.js
@@ -53,7 +53,7 @@ export default function MovieCarousel({ title, endpoint }) {
 
     fetchData();
   }, [endpoint]);
-
+  console.log(endpoint);
   return (
     <section>
       <h3-title>{title}</h3-title>
