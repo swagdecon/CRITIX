7a23087cc10fd01396314e5e9dc4bcfc13c6c3d4
Connor Pant <56475499+swagdecon@users.noreply.github.com>
2023-05-07 17:26:43 +0100

    basic frontend logout working with endpoitns protected

--- frontend/src/components/UserProfile/UserProfile.js
+++ frontend/src/components/UserProfile/UserProfile.js
@@ -1,29 +1,46 @@
 import React from "react";
-import "./UserProfile.css"
+import "./UserProfile.css";
+import { useNavigate } from "react-router-dom";
 
 const UserProfile = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const token = JSON.parse(localStorage.getItem("refreshToken"));
+
+    try {
+      const response = await fetch("http://localhost:8080/v1/auth/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        navigate("/login");
+      }
+    } catch (error) {
+      navigate("/403");
+    }
+  };
   return (
     <div className="UserProfile">
       <div className="User">
         <div className="name">Jack Oliver</div>
-        <div className="image">
-          <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745" />
-        </div>
+        <button className="logout" onClick={handleSubmit}>
+          <div className="image">
+            <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745" />
+          </div>
+        </button>
       </div>
       <div className="UserProfile-menu">
         <div className="UserProfileSwitch">
           <ul>
             <li>
-              <div className="UserProfile-image">
-                <img src="http://lorempixel.com/96/96" />
-              </div>
+              <div className="UserProfile-image" />
               <div className="UserProfile-name">Alexander</div>
-            </li>
-            <li>
-              <div className="UserProfile-image">
-                <img src="http://lorempixel.com/96/96" />
-              </div>
-              <div className="UserProfile-name">Mattias</div>
             </li>
           </ul>
         </div>
