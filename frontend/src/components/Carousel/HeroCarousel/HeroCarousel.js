884004dc7a66b1fa18245b548b438e6bf9cbbba9
Connor Pant <56475499+swagdecon@users.noreply.github.com>
2023-05-10 16:36:17 +0100

    significant hero carousel refactoring

--- frontend/src/components/Carousel/HeroCarousel/HeroCarousel.js
+++ frontend/src/components/Carousel/HeroCarousel/HeroCarousel.js
@@ -6,155 +6,92 @@ import WatchTrailerBtn from "./WatchTrailerbtn";
 import "../../Logo/Loader.js";
 import "./WatchTrailerBtn.css";
 import { Link } from "react-router-dom";
+
+const heroMovies = [
+  {
+    id: 335984,
+    image:
+      "https://www.themoviedb.org/t/p/original/sAtoMqDVhNDQBc3QJL3RF6hlhGq.jpg",
+    title: "BLADE RUNNER 2049",
+    tagline: "THE KEY TO THE FUTURE IS FINALLY UNEARTHED.",
+    genres: ["SCI-FI", "THRILLER", "DRAMA"],
+    trailerUrl: "https://www.youtube.com/watch?v=gCcx85zbxz4",
+  },
+  {
+    id: 330459,
+    image:
+      "https://www.themoviedb.org/t/p/original/6t8ES1d12OzWyCGxBeDYLHoaDrT.jpg",
+    title: "ROGUE ONE: A STAR WARS STORY",
+    tagline: "A REBELLION BUILT ON HOPE.",
+    genres: ["SCI-FI", "ACTION", "ADVENTURE"],
+    trailerUrl: "https://www.youtube.com/watch?v=frdj1zb9sMY",
+  },
+  {
+    id: 157336,
+    image:
+      "https://www.themoviedb.org/t/p/original/xJHokMbljvjADYdit5fK5VQsXEG.jpg",
+    title: "INTERSTELLAR",
+    tagline: "MANKIND WAS BORN ON EARTH. IT WAS NEVER MEANT TO DIE HERE.",
+    genres: ["SCI-FI", "DRAMA", "ADVENTURE"],
+    trailerUrl: "https://www.youtube.com/watch?v=zSWdZVtXT7E",
+  },
+  {
+    id: 27205,
+    image:
+      "https://www.themoviedb.org/t/p/original/s3TBrRGB1iav7gFOCNx3H31MoES.jpg",
+    title: "INCEPTION",
+    tagline: "YOUR MIND IS THE SCENE OF THE CRIME",
+    genres: ["SCI-FI", "ACTION", "ADVENTURE"],
+    trailerUrl: "https://www.youtube.com/watch?v=YoHD9XEInc0",
+  },
+
+  {
+    id: 286217,
+    image:
+      "https://www.themoviedb.org/t/p/original/lzMS0CI3FLQYC5EgJoWeIaEt0lm.jpg",
+    title: "THE MARTIAN",
+    tagline: "BRING HIM HOME.",
+    genres: ["SCI-FI", "DRAMA", "ADVENTURE"],
+    trailerUrl: "https://www.youtube.com/watch?v=ej3ioOneTy8",
+  },
+];
 export default function HeroCarousel() {
+  console.log(heroMovies);
   return (
     <Carousel>
-      <Carousel.Item interval={5000}>
-        <Link to={`/movies/movie/335984`} className="hero-movie-page">
-          <img
-            className="d-block w-100"
-            src="https://www.themoviedb.org/t/p/original/sAtoMqDVhNDQBc3QJL3RF6hlhGq.jpg"
-            alt="First slide"
-          />
-        </Link>
-        <Carousel.Caption>
-          <h1 className="hero_header">BLADE RUNNER 2049</h1>
-          <p className="hero_tagline">
-            THE KEY TO THE FUTURE IS FINALLY UNEARTHED.
-          </p>
-          <div className="hero_genre-container">
-            <p className="hero_genre">SCI-FI</p>
-            <p className="hero_genre">THRILLER</p>
-            <p className="hero_genre">DRAMA</p>
-          </div>
-          <Link
-            className="hero_trailer"
-            href="https://www.youtube.com/watch?v=gCcx85zbxz4"
-          >
-            <button type="button" className="btn btn-warning">
-              <WatchTrailerBtn />
-              WATCH TRAILER
-            </button>
+      {heroMovies.map((movie) => (
+        <Carousel.Item interval={5000} key={movie.id}>
+          <Link to={`/movies/movie/${movie.id}`} className="hero-movie-page">
+            <img
+              className="d-block w-100"
+              src={movie.image}
+              alt={movie.title}
+            />
           </Link>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item interval={5000}>
-        <Link to={`/movies/movie/330459`} className="hero-movie-page">
-          <img
-            className="d-block w-100"
-            src="https://www.themoviedb.org/t/p/original/6t8ES1d12OzWyCGxBeDYLHoaDrT.jpg"
-            alt="Second slide"
-          />
-        </Link>
-
-        <Carousel.Caption>
-          <h1 className="hero_header">ROGUE ONE: A STAR WARS STORY</h1>
-          <p className="hero_tagline">A REBELLION BUILT ON HOPE.</p>
-          <div className="hero_genre-container">
-            <p className="hero_genre">SCI-FI</p>
-            <p className="hero_genre">ACTION</p>
-            <p className="hero_genre">ADVENTURE</p>
-          </div>
-          <Link
-            className="hero_trailer"
-            href="https://www.youtube.com/watch?v=frdj1zb9sMY"
-          >
-            <button type="button" className="btn btn-warning ">
-              <WatchTrailerBtn />
-              WATCH TRAILER
-            </button>
-          </Link>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item interval={5000}>
-        <Link to={`/movies/movie/157336`} className="hero-movie-page">
-          <img
-            className="d-block w-100"
-            src="https://www.themoviedb.org/t/p/original/xJHokMbljvjADYdit5fK5VQsXEG.jpg"
-            alt="Second slide"
-          />
-        </Link>
-
-        <Carousel.Caption>
-          <h1 className="hero_header">INTERSTELLAR</h1>
-          <p className="hero_tagline">
-            MANKIND WAS BORN ON EARTH. IT WAS NEVER MEANT TO DIE HERE.
-          </p>
-          <div className="hero_genre-container">
-            <p className="hero_genre">SCI-FI</p>
-            <p className="hero_genre">DRAMA</p>
-            <p className="hero_genre">ADVENTURE</p>
-          </div>
-          <Link
-            className="hero_trailer"
-            href="https://www.youtube.com/watch?v=zSWdZVtXT7E"
-          >
-            <button type="button" className="btn btn-warning ">
-              <WatchTrailerBtn />
-              WATCH TRAILER
-            </button>
-          </Link>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item interval={5000}>
-        <Link to={`/movies/movie/286217`} className="hero-movie-page">
-          <img
-            className="d-block w-100"
-            src="https://www.themoviedb.org/t/p/original/lzMS0CI3FLQYC5EgJoWeIaEt0lm.jpg"
-            alt="Fourth slide"
-          />
-        </Link>
-
-        <Carousel.Caption>
-          <h1 className="hero_header">THE MARTIAN</h1>
-          <p className="hero_tagline">BRING HIM HOME.</p>
-          <div className="hero_genre-container">
-            <p className="hero_genre">SCI-FI</p>
-            <p className="hero_genre">DRAMA</p>
-            <p className="hero_genre">ADVENTURE</p>
-          </div>
-          <Link
-            className="hero_trailer"
-            href="https://www.youtube.com/watch?v=ej3ioOneTy8"
-          >
-            <button type="button" className="btn btn-warning">
-              <WatchTrailerBtn />
-              WATCH TRAILER
-            </button>
-          </Link>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item interval={5000}>
-        <Link to={`/movies/movie/27205`} className="hero-movie-page">
-          <img
-            className="d-block w-100"
-            src="https://www.themoviedb.org/t/p/original/s3TBrRGB1iav7gFOCNx3H31MoES.jpg"
-            alt="Fifth slide"
-          />
-        </Link>
-        <Carousel.Caption>
-          <h1 className="hero_header">INCEPTION</h1>
-          <p className="hero_tagline">YOUR MIND IS THE SCENE OF THE CRIME.</p>
-          <div className="hero_genre-container">
-            <p className="hero_genre">SCI-FI</p>
-            <p className="hero_genre">ACTION</p>
-            <p className="hero_genre">ADVENTURE</p>
-          </div>
-          <Link
-            className="hero_trailer"
-            href="https://www.youtube.com/watch?v=YoHD9XEInc0"
-          >
-            <button type="button" className="btn btn-warning">
-              <WatchTrailerBtn />
-              WATCH TRAILER
-            </button>
-          </Link>
-        </Carousel.Caption>
-      </Carousel.Item>
+          <Carousel.Caption>
+            <h1 className="hero_header">{movie.title}</h1>
+            <p className="hero_tagline">{movie.tagline}</p>
+            <div className="hero_genre-container">
+              {movie.genres.map((genre) => (
+                <p className="hero_genre" key={genre}>
+                  {genre}
+                </p>
+              ))}
+            </div>
+            <Link
+              className="hero_trailer"
+              to={movie.trailerUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button type="button" className="btn btn-warning ">
+                <WatchTrailerBtn />
+                WATCH TRAILER
+              </button>
+            </Link>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 }
