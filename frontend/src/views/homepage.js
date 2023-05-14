da987916ee15ea758965a670b7e1384440771759
Connor Pant <56475499+swagdecon@users.noreply.github.com>
2023-05-14 19:09:41 +0100

    added loading page to homepage

--- frontend/src/views/homepage.js
+++ frontend/src/views/homepage.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "../components/MovieCard/moviecard.module.scss";
 import HeroCarousel from "../components/Carousel/HeroCarousel/HeroCarousel";
 import MovieCarousel from "../components/Carousel/MovieCarousel/MovieCarousel.js";
 import NavBar from "../components/NavBar/NavBar.js";
-
+import LoadingPage from "./LoadingPage";
 export default function Homepage() {
-  return (
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoad = () => {
+    setIsLoading(false);
+  };
+  return !isLoading ? (
+    <LoadingPage />
+  ) : (
     <div>
       <NavBar />
-      <HeroCarousel />
-      <MovieCarousel title="Popular right now" endpoint="/movies/popular" />
-      <MovieCarousel title="Top Rated" endpoint="/movies/top_rated" />
-      <MovieCarousel title="Releasing Soon" endpoint="/movies/upcoming" />
+      <HeroCarousel onLoad={handleLoad} />
+      <MovieCarousel
+        title="Popular right now"
+        endpoint="/movies/popular"
+        onLoad={handleLoad}
+      />
+      <MovieCarousel
+        title="Top Rated"
+        endpoint="/movies/top_rated"
+        onLoad={handleLoad}
+      />
+      <MovieCarousel
+        title="Releasing Soon"
+        endpoint="/movies/upcoming"
+        onLoad={handleLoad}
+      />
     </div>
   );
 }
