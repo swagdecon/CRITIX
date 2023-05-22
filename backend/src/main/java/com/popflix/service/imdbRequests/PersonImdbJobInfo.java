4e7c4ed700f9f560b3e33572a8d549aa7ddaa9eb
Connor Pant <56475499+swagdecon@users.noreply.github.com>
2023-05-22 19:22:20 +0100

    person backend logic update

--- backend/src/main/java/com/popflix/service/imdbRequests/PersonImdbJobInfo.java
+++ backend/src/main/java/com/popflix/service/imdbRequests/PersonImdbJobInfo.java
@@ -57,4 +57,21 @@ public class PersonImdbJobInfo {
         }
         return count;
     }
+
+    public Integer personAwardNominations(String imdbId) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://imdb8.p.rapidapi.com/actors/get-awards-summary?nconst=" + imdbId))
+                .header("X-RapidAPI-Key", "349136b942msh6c632fc990fca08p1625c1jsn9838599cbe73")
+                .header("X-RapidAPI-Host", "imdb8.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(response.body());
+        JsonNode awardsNode = rootNode.get("awardsSummary");
+        Integer otherNominationsCount = awardsNode.get("otherNominationsCount").asInt();
+
+        return otherNominationsCount;
+    }
 }
