b23f9e941f7d8cac94c959dafee54c8f7cc7ac52
Connor Pant <56475499+swagdecon@users.noreply.github.com>
2023-06-10 10:45:20 +0100

    added rating circle *not functional*

--- frontend/src/components/Review/MovieReviews.js
+++ frontend/src/components/Review/MovieReviews.js
@@ -4,14 +4,21 @@ import TextField from '@mui/material/TextField'
 import Filter from "bad-words";
 import Button from "@mui/material/Button"
 import MovieCreationOutlinedIcon from '@mui/icons-material/MovieCreationOutlined';
-import CustomizedRating from "./Rating";
+import UserRating from "./Rating/UserRating";
 import PropTypes from "prop-types";
-import { MovieAverage } from "../IndMovie/MovieComponents";
-
+// import { MovieAverage } from "../IndMovie/MovieComponents";
+// import CookieManager from "../../security/CookieManager";
+// import jwt_decode from "jwt-decode";
+import PercentageRatingCircle from "./Rating/PercentageCircle";
 export default function MovieComments({ voteAverage }) {
     MovieComments.propTypes = {
         voteAverage: PropTypes.number,
     }
+    const percentageAverage = voteAverage * 10
+    console.log(percentageAverage);
+    // const token = CookieManager.decryptCookie("accessToken");
+    // const firstName = jwt_decode(token).firstName;
+
     const [review, setReview] = useState("");
     const filter = new Filter();
     const hasReviewProfanity = filter.isProfane(review);
@@ -21,65 +28,71 @@ export default function MovieComments({ voteAverage }) {
 
             < div className={IndReview["review-content-wrapper"]}>
                 <div className={IndReview["movie-vote-average"]}>
-                    <MovieAverage voteAverage={voteAverage} />
+                    <PercentageRatingCircle percentageRating={percentageAverage} />
+                    {/* <MovieAverage voteAverage={voteAverage} /> */}
                 </div>
                 <div className={IndReview["input-wrapper"]}>
 
                     <div className={IndReview["rating-wrapper"]}>
-                        <CustomizedRating />
+                        <UserRating />
                     </div>
-                    <TextField
-                        id="outlined-multiline-flexible"
-                        label={hasReviewProfanity && review.trim().length > 0 ? "Profanity is not allowed." : "Post A Review"}
-                        multiline
-                        onChange={(e) => setReview(e.target.value)}
-                        maxRows={4}
-                        className={IndReview["mui-input-field"]}
-                        InputLabelProps={{
-                            style: {
-                                color: 'white',
-                            },
-                        }}
-                        inputProps={{
-                            sx: {
-                                color: 'white',
+                    {/* <div className={IndReview["name-wrapper"]}>
+                        {firstName}
+                    </div> */}
+                    <div className={IndReview["user-review-wrapper"]}>
+                        <TextField
+                            id="outlined-multiline-flexible"
+                            label={hasReviewProfanity && review.trim().length > 0 ? "Profanity is not allowed." : "Post A Review"}
+                            multiline
+                            onChange={(e) => setReview(e.target.value)}
+                            maxRows={4}
+                            className={IndReview["mui-input-field"]}
+                            InputLabelProps={{
+                                style: {
+                                    color: 'white',
+                                },
+                            }}
+                            inputProps={{
+                                sx: {
+                                    color: 'white',
 
-                            },
-                        }}
-                        error={hasReviewProfanity && review.trim().length > 0}
-                        sx={{
-                            borderRadius: "30px",
-                            fieldSet: {
-                                borderRadius: "30px",
-                            },
-                            input: {
-                                color: "white !important"
-                            },
-                            '& .MuiOutlinedInput-root': {
-                                '& fieldset': {
-                                    borderColor: 'white',
                                 },
-                                '&:hover fieldset': {
-                                    borderColor: hasReviewProfanity && review.trim().length > 0 ? '#ff0000' : '#0096ff',
-                                },
-                                '&.Mui-focused fieldset': {
-                                    borderColor: hasReviewProfanity && review.trim().length > 0 ? '#ff0000' : '#0096ff',
-                                },
-                            },
-                            width: "65%",
-                        }}
-                    />
-                    <div className={IndReview["post-review-btn"]}>
-                        <Button
-                            variant="contained"
-                            endIcon={<MovieCreationOutlinedIcon />}
-                            size="large"
+                            }}
+                            error={hasReviewProfanity && review.trim().length > 0}
                             sx={{
                                 borderRadius: "30px",
+                                fieldSet: {
+                                    borderRadius: "30px",
+                                },
+                                input: {
+                                    color: "white !important"
+                                },
+                                '& .MuiOutlinedInput-root': {
+                                    '& fieldset': {
+                                        borderColor: 'white',
+                                    },
+                                    '&:hover fieldset': {
+                                        borderColor: hasReviewProfanity && review.trim().length > 0 ? '#ff0000' : '#0096ff',
+                                    },
+                                    '&.Mui-focused fieldset': {
+                                        borderColor: hasReviewProfanity && review.trim().length > 0 ? '#ff0000' : '#0096ff',
+                                    },
+                                },
+                                width: "65%",
                             }}
-                        >
-                            POST REVIEW
-                        </Button>
+                        />
+                        <div className={IndReview["post-review-btn"]}>
+                            <Button
+                                variant="contained"
+                                endIcon={<MovieCreationOutlinedIcon />}
+                                size="large"
+                                sx={{
+                                    borderRadius: "30px",
+                                }}
+                            >
+                                POST REVIEW
+                            </Button>
+                        </div>
                     </div>
                 </div>
             </div>
--- frontend/src/components/Review/Rating/UserRating.js
+++ frontend/src/components/Review/Rating/UserRating.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Rating from '@mui/material/Rating';
-import IndReview from "./Review.module.css";
+import IndReview from "../Review.module.css";
 import MovieCreationIcon from '@mui/icons-material/MovieCreation';
 import MovieCreationOutlinedIcon from '@mui/icons-material/MovieCreationOutlined';
 const StyledRating = styled(Rating)({
@@ -13,12 +13,12 @@ const StyledRating = styled(Rating)({
     },
 });
 
-export default function CustomizedRating() {
+export default function UserRating() {
     return (
         <div className={IndReview["rating-wrapper"]}>
             <StyledRating
                 name="customized-color"
-                defaultValue={2}
+                defaultValue={1}
                 // getLabelText={(value) => `${value} Clapperboards ${value !== 1 ? 's' : ''}`}
                 precision={0.5}
                 icon={<MovieCreationIcon style={{ color: "#FFD700" }} fontSize="large" />}
