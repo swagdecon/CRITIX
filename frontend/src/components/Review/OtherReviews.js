2a96e9d8a4aef380c692eeca5c7ef5148277caba
Connor Pant <56475499+swagdecon@users.noreply.github.com>
2023-06-15 20:45:33 +0100

    making progress with rating design

--- frontend/src/components/Review/OtherReviews.js
+++ frontend/src/components/Review/OtherReviews.js
@@ -5,7 +5,28 @@ export default function OtherReviews() {
         <div className="comment-section">
             <div className="container">
                 <div className="review">
+                    <h2 className="R-title">Reviews</h2>
                     <div className="comment-section">
+
+                        <div className="media media-review">
+
+                            <div className="media-user"><img src="https://i.imgur.com/nUNhspp.jpg" alt="" /> </div>
+                            <div className="media-body">
+                                <div className="M-flex">
+                                    <h4 className="title"><span> Robert Bye </span>DD-MM-YYYY</h4>
+                                    <div className="rating-row">
+                                        <ul>
+                                            <li className=""><i className="fa fa-star"></i></li>
+                                            <li className=""><i className="fa fa-star"></i></li>
+                                            <li className=""><i className="fa fa-star"></i></li>
+                                            <li className=""><i className="fa fa-star-o"></i></li>
+                                            <li className=""><i className="fa fa-star-o"></i></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </div>
+                            </div>
+                        </div>
                         <div className="media media-review">
                             <div className="media-user"><img src="https://i.imgur.com/nUNhspp.jpg" alt="" /></div>
                             <div className="media-body">
@@ -22,9 +43,28 @@ export default function OtherReviews() {
                                     </div>
                                 </div>
                                 <div className="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </div>
+
                             </div>
                         </div>
+                        <div className="media media-review">
+                            <div className="media-user"><img src="https://i.imgur.com/nUNhspp.jpg" alt="" /></div>
+                            <div className="media-body">
+                                <div className="M-flex">
+                                    <h2 className="title"><span> Robert Bye </span>DD-MM-YYYY</h2>
+                                    <div className="rating-row">
+                                        <ul>
+                                            <li className=""><i className="fa fa-star"></i></li>
+                                            <li className=""><i className="fa fa-star"></i></li>
+                                            <li className=""><i className="fa fa-star"></i></li>
+                                            <li className=""><i className="fa fa-star-o"></i></li>
+                                            <li className=""><i className="fa fa-star-o"></i></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </div>
 
+                            </div>
+                        </div>
 
                     </div>
 
