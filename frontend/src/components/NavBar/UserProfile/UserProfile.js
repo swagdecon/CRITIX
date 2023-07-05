e33ba4ae0225a349ca3f7dea85c892e91c53df5c
Connor Pant <56475499+swagdecon@users.noreply.github.com>
2023-07-05 19:51:30 +0100

    user first name now visible in header

--- frontend/src/components/NavBar/UserProfile/UserProfile.js
+++ frontend/src/components/NavBar/UserProfile/UserProfile.js
@@ -1,8 +1,9 @@
-import React from "react";
+import { React, useMemo } from "react";
 import "./UserProfile.css";
 import Logout from "../../../security/Logout";
 import { useNavigate } from "react-router-dom";
-
+import jwt_decode from "jwt-decode";
+import CookieManager from "../../../security/CookieManager";
 const UserProfile = () => {
   const navigate = useNavigate();
 
@@ -10,18 +11,20 @@ const UserProfile = () => {
     event.preventDefault();
     await Logout(navigate);
   };
-
+  const token = useMemo(() => CookieManager.decryptCookie("accessToken"), []);
+  const decodedToken = useMemo(() => jwt_decode(token), [token]);
+  const firstName = decodedToken.firstName
   return (
     <div className="UserProfile">
       <div className="User">
-        <div className="name">Jack Oliver</div>
+        <div className="name">{firstName}</div>
         <button className="logout" onClick={handleSubmit}>
           <div className="image">
             <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745" />
           </div>
         </button>
       </div>
-      <div className="UserProfile-menu">
+      {/* <div className="UserProfile-menu">
         <div className="UserProfileSwitch">
           <ul>
             <li>
@@ -34,10 +37,10 @@ const UserProfile = () => {
           <ul>
             <li>Your Account</li>
             <li>Help Center</li>
-            <li>Sign out of Netflix</li>
+            <li>Sign out of Popflix</li>
           </ul>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
