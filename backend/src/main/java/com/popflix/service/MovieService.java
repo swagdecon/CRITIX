5d81a458abb4032b1233588ac86a72604089232a
Connor Pant <56475499+swagdecon@users.noreply.github.com>
2023-07-19 17:41:07 +0100

    service refactoring for performance

--- backend/src/main/java/com/popflix/service/MovieService.java
+++ backend/src/main/java/com/popflix/service/MovieService.java
@@ -3,9 +3,13 @@ package com.popflix.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -32,20 +36,20 @@ public class MovieService {
   Dotenv dotenv = Dotenv.load();
   private String TMDB_API_KEY = dotenv.get("TMDB_API_KEY");
   private final MovieRepository movieRepository;
+  private final MongoTemplate mongoTemplate;
+  private final TmdbApi tmdbApi;
+  private ScheduledExecutorService executor;
+
+  @Autowired
+  public MovieService(MovieRepository movieRepository, MongoTemplate mongoTemplate) {
+    this.movieRepository = movieRepository;
+    this.mongoTemplate = mongoTemplate;
+    this.tmdbApi = new TmdbApi(TMDB_API_KEY);
+  }
 
   public Optional<Movie> findMovieById(Integer id) {
     return movieRepository.findMovieById(id);
   }
-
-  public MovieService(MovieRepository movieRepository, MongoTemplate mongoTemplate) {
-    this.movieRepository = movieRepository;
-    this.mongoTemplate = mongoTemplate;
-
-  }
-
-  @Autowired
-  private MongoTemplate mongoTemplate;
-  private final TmdbApi tmdbApi = new TmdbApi(TMDB_API_KEY);
 
   public List<Movie> allMovies(String collectionName) {
     Query query = new Query();
@@ -61,9 +65,7 @@ public class MovieService {
   public Optional<Movie> singleTmdbMovie(Integer id) {
     Movie movie = new Movie();
     movie.setId(id);
-
     MovieDb movieDb = tmdbApi.getMovies().getMovie(movie.getId(), "en-US");
-
     movie.setAdult(movieDb.isAdult());
     movie.setTitle(movieDb.getOriginalTitle());
     movie.setOriginalLanguage(movieDb.getOriginalLanguage());
@@ -80,11 +82,6 @@ public class MovieService {
     return Optional.of(movie);
   }
 
-  // ScheduledExecutorService is created in the init method and a task is
-  // scheduled to run every 24 hours using scheduleAtFixedRate. The updateMovies
-  // method will be called by the scheduled task.
-  private ScheduledExecutorService executor;
-
   @PostConstruct
   public void init() {
     executor = Executors.newScheduledThreadPool(1);
@@ -96,133 +93,112 @@ public class MovieService {
   }
 
   public void updateMovies() {
-    saveMovieDetails("now_playing");
-    saveMovieDetails("popular");
-    saveMovieDetails("top_rated");
-    saveMovieDetails("upcoming_movies");
+    CompletableFuture<Void> nowPlayingFuture = saveMovieDetails("now_playing");
+    CompletableFuture<Void> popularFuture = saveMovieDetails("popular");
+    CompletableFuture<Void> topRatedFuture = saveMovieDetails("top_rated");
+    CompletableFuture<Void> upcomingFuture = saveMovieDetails("upcoming_movies");
 
-    updateMovieDetails("now_playing");
-    updateMovieDetails("popular");
-    updateMovieDetails("top_rated");
-    updateMovieDetails("upcoming_movies");
+    try {
+      CompletableFuture.allOf(nowPlayingFuture, popularFuture, topRatedFuture, upcomingFuture).get();
+    } catch (InterruptedException | ExecutionException e) {
+      System.out.println(e);
+    }
 
+    CompletableFuture<Void> updateNowPlayingFuture = updateMovieDetails("now_playing");
+    CompletableFuture<Void> updatePopularFuture = updateMovieDetails("popular");
+    CompletableFuture<Void> updateTopRatedFuture = updateMovieDetails("top_rated");
+    CompletableFuture<Void> updateUpcomingFuture = updateMovieDetails("upcoming_movies");
+
+    try {
+      CompletableFuture.allOf(updateNowPlayingFuture, updatePopularFuture, updateTopRatedFuture, updateUpcomingFuture)
+          .get();
+    } catch (InterruptedException | ExecutionException e) {
+      System.out.println(e);
+    }
   }
 
-  public void updateMovieDetails(String collectionName) {
-    List<Movie> movies = allMovies(collectionName);
-    for (Movie movie : movies) {
-      MovieDb movieDb = tmdbApi.getMovies().getMovie(movie.getId(), "en-US");
+  public CompletableFuture<Void> updateMovieDetails(String collectionName) {
+    return CompletableFuture.runAsync(() -> {
+      List<Movie> movies = allMovies(collectionName);
+      for (Movie movie : movies) {
+        MovieDb movieDb = tmdbApi.getMovies().getMovie(movie.getId(), "en-US");
+        movie.setBudget(movie.getBudget() != null ? movie.getBudget() : movieDb.getBudget());
+        movie.setTagline(movie.getTagline() != null ? movie.getTagline() : movieDb.getTagline());
+        movie.setRevenue(movie.getRevenue() != null ? movie.getRevenue() : movieDb.getRevenue());
+        movie.setRuntime(movie.getRuntime() != null ? movie.getRuntime() : movieDb.getRuntime());
+        movie.setVoteAverage(movie.getVoteAverage() != null ? movie.getVoteAverage() : movieDb.getVoteAverage());
+        movie.setVoteCount(movie.getVoteCount() != null ? movie.getVoteCount() : movieDb.getVoteCount());
+        movie.setImdbId(movie.getImdbId() != null ? movie.getImdbId() : movieDb.getImdbID());
 
-      // Initialize fields if they don't exist
-      if (movie.getBudget() == null) {
-        movie.setBudget(movieDb.getBudget());
-      }
-      if (movie.getTagline() == null) {
-        movie.setTagline(movieDb.getTagline());
-      }
-      if (movie.getRevenue() == null) {
-        movie.setRevenue(movieDb.getRevenue());
-      }
-      if (movie.getRuntime() == null) {
-        movie.setRuntime(movieDb.getRuntime());
-      }
-      if (movie.getGenres() == null || movie.getGenres().isEmpty()) {
         // Get the movie genres
-        List<Genre> genres = movieDb.getGenres();
-
-        // Extract the movie genre names
-        List<String> genreNames = new ArrayList<>();
-        for (Genre genre : genres) {
-          genreNames.add(genre.getName());
-        }
-        movie.setGenres(genreNames);
-      }
-
-      if (movie.getActors() == null || movie.getActors().isEmpty() ||
-          movie.getActorImagePaths() == null || movie.getActorImagePaths().isEmpty()) {
-        Credits movieCredits = tmdbApi.getMovies().getCredits(movie.getId());
-        List<PersonCast> castList = movieCredits.getCast();
-        List<Person> actors = new ArrayList<>();
-        for (PersonCast cast : castList) {
-          Person person = new Person();
-          // This sets the Id of the person to the PersonId, not the CastId
-          person.setId(cast.getId());
-          person.setName(cast.getName());
-          person.setProfilePath(cast.getProfilePath());
-          actors.add(person);
-        }
-        movie.setActors(actors);
-      }
-
-      if (movie.getReviews() == null || movie.getReviews().isEmpty()) {
-        List<Reviews> reviews = tmdbApi.getReviews().getReviews(movie.getId(), "en-US", 1).getResults();
-
-        // Extract the review text for each review
-        List<String> reviewTexts = new ArrayList<>();
-        for (Reviews review : reviews) {
-          String content = review.getContent();
-          if (content.split("\\s+").length < 300 && !content.contains("SPOILER-FREE")
-              && content.split("\\s+").length > 20) {
-            reviewTexts.add(content);
-          }
+        if (movie.getGenres() == null || movie.getGenres().isEmpty()) {
+          List<Genre> genres = movieDb.getGenres();
+          List<String> genreNames = genres.stream().map(Genre::getName).collect(Collectors.toList());
+          movie.setGenres(genreNames);
         }
 
-        movie.setReviews(reviewTexts);
-      }
-      if (movie.getVoteAverage() == null) {
-        float voteAverage = movieDb.getVoteAverage();
-        movie.setVoteAverage(voteAverage);
-      }
-      if (movie.getVoteCount() == null) {
-        Integer voteCount = movieDb.getVoteCount();
-        movie.setVoteCount(voteCount);
-      }
+        // Get the movie actors
+        if (movie.getActors() == null || movie.getActors().isEmpty() ||
+            movie.getActorImagePaths() == null || movie.getActorImagePaths().isEmpty()) {
+          Credits movieCredits = tmdbApi.getMovies().getCredits(movie.getId());
+          List<PersonCast> castList = movieCredits.getCast();
+          List<Person> actors = castList.stream()
+              .map(cast -> {
+                Person person = new Person();
+                person.setId(cast.getId());
+                person.setName(cast.getName());
+                person.setProfilePath(cast.getProfilePath());
+                return person;
+              })
+              .collect(Collectors.toList());
+          movie.setActors(actors);
+        }
 
-      if (movie.getImdbId() == null) {
-        String imdbId = movieDb.getImdbID();
+        // Get the movie reviews
+        if (movie.getReviews() == null || movie.getReviews().isEmpty()) {
+          List<Reviews> reviews = tmdbApi.getReviews().getReviews(movie.getId(), "en-US", 1).getResults();
+          List<String> reviewTexts = reviews.stream()
+              .filter(review -> review.getContent().split("\\s+").length < 300
+                  && !review.getContent().contains("SPOILER-FREE")
+                  && review.getContent().split("\\s+").length > 20)
+              .map(Reviews::getContent)
+              .collect(Collectors.toList());
+          movie.setReviews(reviewTexts);
+        }
 
-        movie.setImdbId(imdbId);
-      }
-
-      if (movie.getProductionCompanies() == null || movie.getProductionCompanies().isEmpty()) {
         // Get the movie production companies
-        List<ProductionCompany> productionCompaniesList = movieDb.getProductionCompanies();
-
-        // Extract the movie production company names
-        List<String> productionCompanies = new ArrayList<>();
-        for (ProductionCompany productionCompany : productionCompaniesList) {
-          productionCompanies.add(productionCompany.getName());
+        if (movie.getProductionCompanies() == null || movie.getProductionCompanies().isEmpty()) {
+          List<ProductionCompany> productionCompaniesList = movieDb.getProductionCompanies();
+          List<String> productionCompanies = productionCompaniesList.stream()
+              .map(ProductionCompany::getName)
+              .collect(Collectors.toList());
+          movie.setProductionCompanies(productionCompanies);
         }
-        movie.setProductionCompanies(productionCompanies);
-      }
 
-      if (movie.getMovieStatus() == null || movie.getMovieStatus().isEmpty()) {
-        String movieStatus = tmdbApi.getMovies().getMovie(movie.getId(), "en-US").getStatus();
-        movie.setMovieStatus(movieStatus);
-      }
+        // Get the movie status
+        if (movie.getMovieStatus() == null || movie.getMovieStatus().isEmpty()) {
+          String movieStatus = tmdbApi.getMovies().getMovie(movie.getId(), "en-US").getStatus();
+          movie.setMovieStatus(movieStatus);
+        }
 
-      if (movie.getVideo() == null || movie.getVideo().isEmpty()) {
-        List<Video> movieVideos = tmdbApi.getMovies().getVideos(movie.getId(), "en-US");
-
-        // Extract the video key for the main trailer
-        String mainTrailerKey = null;
-        for (Video video : movieVideos) {
-          if (video.getType().equals("Trailer") && video.getSite().equals("YouTube")) {
-            mainTrailerKey = video.getKey();
-            break;
+        // Get the movie videos
+        if (movie.getVideo() == null || movie.getVideo().isEmpty()) {
+          List<Video> movieVideos = tmdbApi.getMovies().getVideos(movie.getId(), "en-US");
+          String mainTrailerKey = movieVideos.stream()
+              .filter(video -> video.getType().equals("Trailer") && video.getSite().equals("YouTube"))
+              .map(Video::getKey)
+              .findFirst()
+              .orElse(null);
+          if (mainTrailerKey != null) {
+            List<String> videoKeys = new ArrayList<>();
+            videoKeys.add(mainTrailerKey);
+            movie.setVideo(videoKeys);
           }
         }
-
-        // Set the main trailer video key for the movie
-        if (mainTrailerKey != null) {
-          List<String> videoKeys = new ArrayList<>();
-          videoKeys.add(mainTrailerKey);
-          movie.setVideo(videoKeys);
-        }
       }
 
-      mongoTemplate.save(movie, collectionName);
-    }
+      mongoTemplate.save(movies, collectionName);
+    });
   }
 
   public void updateTmdbMovieDetails(Movie movie) {
@@ -322,44 +298,49 @@ public class MovieService {
     }
   }
 
-  public void saveMovieDetails(String collectionName) {
-    List<Movie> movies = new ArrayList<>();
-    List<MovieDb> collection = null;
-    mongoTemplate.dropCollection(collectionName);
-    switch (collectionName) {
-      case "now_playing":
-        collection = tmdbApi.getMovies().getNowPlayingMovies("en-US", 1, "").getResults();
-        break;
-      case "popular":
-        collection = tmdbApi.getMovies().getPopularMovies("en-US", 1).getResults();
-        break;
-      case "top_rated":
-        collection = tmdbApi.getMovies().getTopRatedMovies("en-US", 1).getResults();
-        break;
-      case "upcoming_movies":
-        collection = tmdbApi.getMovies().getUpcoming("en-US", 1, "").getResults();
-        break;
-      default:
-        throw new IllegalArgumentException("Invalid method name: " + collectionName);
-    }
-    for (MovieDb movieDb : collection) {
-      Movie movie = new Movie();
-      movie.setId(movieDb.getId());
-      movie.setAdult(movieDb.isAdult());
-      movie.setTitle(movieDb.getOriginalTitle());
-      movie.setOriginalLanguage(movieDb.getOriginalLanguage());
-      movie.setOverview(movieDb.getOverview());
-      movie.setPopularity(movieDb.getPopularity());
-      movie.setPosterPath(movieDb.getPosterPath());
-      movie.setBackdropPath(movieDb.getBackdropPath());
-      movie.setReleaseDate(movieDb.getReleaseDate());
-      movie.setVideo(movie.getVideo());
-      movie.setVoteAverage(movie.getVoteAverage());
-      movie.setVoteCount(movie.getVoteCount());
-      movies.add(movie);
-    }
-    if (!movies.isEmpty()) {
-      mongoTemplate.insert(movies, collectionName);
-    }
+  public CompletableFuture<Void> saveMovieDetails(String collectionName) {
+    return CompletableFuture.runAsync(() -> {
+      List<Movie> movies = new ArrayList<>();
+      List<MovieDb> collection = null;
+      mongoTemplate.dropCollection(collectionName);
+
+      switch (collectionName) {
+        case "now_playing":
+          collection = tmdbApi.getMovies().getNowPlayingMovies("en-US", 1, "").getResults();
+          break;
+        case "popular":
+          collection = tmdbApi.getMovies().getPopularMovies("en-US", 1).getResults();
+          break;
+        case "top_rated":
+          collection = tmdbApi.getMovies().getTopRatedMovies("en-US", 1).getResults();
+          break;
+        case "upcoming_movies":
+          collection = tmdbApi.getMovies().getUpcoming("en-US", 1, "").getResults();
+          break;
+        default:
+          throw new IllegalArgumentException("Invalid method name: " + collectionName);
+      }
+
+      for (MovieDb movieDb : collection) {
+        Movie movie = new Movie();
+        movie.setId(movieDb.getId());
+        movie.setAdult(movieDb.isAdult());
+        movie.setTitle(movieDb.getOriginalTitle());
+        movie.setOriginalLanguage(movieDb.getOriginalLanguage());
+        movie.setOverview(movieDb.getOverview());
+        movie.setPopularity(movieDb.getPopularity());
+        movie.setPosterPath(movieDb.getPosterPath());
+        movie.setBackdropPath(movieDb.getBackdropPath());
+        movie.setReleaseDate(movieDb.getReleaseDate());
+        movie.setVideo(movie.getVideo());
+        movie.setVoteAverage(movie.getVoteAverage());
+        movie.setVoteCount(movie.getVoteCount());
+        movies.add(movie);
+      }
+
+      if (!movies.isEmpty()) {
+        mongoTemplate.insert(movies, collectionName);
+      }
+    });
   }
 }
