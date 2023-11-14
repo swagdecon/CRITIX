3090c68cba8a8d48148046b85c8a5af4ff461b8c
Connor Pant <swagdecon77@gmail.com>
2023-11-14 19:41:45 +0000

    minor css fixes

--- backend/src/main/java/com/popflix/service/PasswordRecoveryService.java
+++ backend/src/main/java/com/popflix/service/PasswordRecoveryService.java
@@ -17,12 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.security.access.method.P;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.popflix.config.customExceptions.BadRequestException;
 import com.popflix.config.customExceptions.TokenExpiredException;
 import com.popflix.config.customExceptions.TooManyRequestsException;
 import com.popflix.model.User;
