0d815271bab030f988a480a049d78eef7ad7f98a
Connor Pant <swagdecon77@gmail.com>
2025-10-11 16:43:51 +0100

    user profile css improvements

--- backend/src/main/java/com/critix/controller/PaymentController.java
+++ backend/src/main/java/com/critix/controller/PaymentController.java
@@ -3,6 +3,10 @@ package com.critix.controller;
 import com.stripe.model.checkout.Session;
 import com.stripe.model.Event;
 import com.stripe.net.Webhook;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import com.critix.config.EnvLoader;
 import com.critix.repository.UserRepository;
 import com.stripe.exception.SignatureVerificationException;
 
@@ -22,35 +26,44 @@ public class PaymentController {
     @Autowired
     private UserRepository userRepository;
 
-    private final String endpointSecret = "whsec_xxx"; // 👈 Your Stripe webhook secret
+    private EnvLoader envLoader = new EnvLoader();
+    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+    private String STRIPE_API = envLoader.getEnv("STRIPE_KEY", dotenv);
 
     @PostMapping("/webhook")
     public ResponseEntity<String> handleStripeWebhook(HttpServletRequest request) {
-        String payload = "";
+        System.out.println(STRIPE_API);
+        String payload;
         try (Scanner scanner = new Scanner(request.getInputStream(), "UTF-8")) {
             payload = scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "";
+            System.out.println("GOING HERE 2");
+
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error reading payload");
         }
 
         String sigHeader = request.getHeader("Stripe-Signature");
         Event event;
+        System.out.println("GOING HERE 3");
 
         try {
-            event = Webhook.constructEvent(payload, sigHeader, endpointSecret);
+            event = Webhook.constructEvent(payload, sigHeader, STRIPE_API);
         } catch (SignatureVerificationException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid signature");
         }
+        System.out.println("GOING HERE 4");
 
-        // ✅ Handle successful checkout
         if ("checkout.session.completed".equals(event.getType())) {
             Session session = (Session) event.getDataObjectDeserializer()
                     .getObject()
                     .map(obj -> (Session) obj)
                     .orElse(null);
+            System.out.println("GOING HERE 5");
 
             if (session != null) {
-                String userId = session.getClientReferenceId(); // 👈 from your frontend
+                String userId = session.getClientReferenceId();
+                System.out.println(session.getClientReferenceId());
+                System.out.println("GOING HERE 6");
                 if (userId != null) {
                     userRepository.findById(userId).ifPresent(user -> {
                         user.setIsUltimateUser(true);
